31c89e553f22f4db55285f7a222d226ab77859f0
Lilis Elisa <liliels555@gmail.com>
2025-09-24 15:51:55 +0700

    first commit

---
+++ pertemuan-01-setup/README.md
@@ -0,0 +1,7 @@
+numpy: 2.0.1
+pandas: 2.3.2
+sklearn: 1.7.1
+skfuzzy: 0.5.0
+opencv: 4.10.0
+torch:  2.5.1
+tensorflow: 2.20.0
---
+++ pertemuan-01-setup/verify_installation.py
@@ -0,0 +1,13 @@
+#verify_installation
+
+import sys 
+import importlib
+packages = ["numpy","pandas","sklearn","skfuzzy","cv2","matplotlib","nltk"]
+for pkg in packages:
+    try:
+        m = importlib.import_module(pkg)
+        print(f"(pkg): OK, version = {getattr(m, '__version__', 'unknown')}")
+    except Exception as e:
+        print(f"(pkg):ERROR -> (e)")
+
+print("Python:", sys.version)
